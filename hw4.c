4e70ca068e282d72c09234d9a3f1a2db6e4a809e
LeeGyoengJin <78292636+LeeGyoengJin@users.noreply.github.com>
2022-09-29 15:26:08 +0900

    Add files via upload

---
+++ hw4.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int num;
+	int count = 0;
+	printf("양의 정수 입력 : ");
+	scanf("%d", &num);
+	
+	for (int i = 2; i < num; i++)
+	{
+		if (num % i != 0)
+			continue;
+		else
+		{
+			count += 1;
+			break;
+		}
+	}
+	switch (count)
+	{
+	case 0:
+		printf("It is a prime number.");
+		break;
+	default:
+		printf("It is not a prime number.");
+		break;
+	}
+	return 0;
+}
